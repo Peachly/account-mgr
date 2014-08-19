c3665dd7fb1339a12a4b056b2907a3f7ee96eae1
Websiteca Dev <support@websites.ca>
2014-08-19 10:11:42 -0500

    added images to public site

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ var swig = require('swig');
 var path = require('path');
 
 var app = express();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+app.set('view cache', false);
+swig.setDefaults({ cache: false });
 
 app.get('/', function(req, res){
-	var filepath = path.join(__dirname, 'tpl/index.html');
-	var tpl = swig.compileFile(filepath);
-	res.send(tpl());
+	res.render('index', { /* template locals context */ });
 });
 
 app.use("/bower", express.static(path.join(__dirname, 'bower_components')));
+app.use("/public", express.static(path.join(__dirname, 'public')));
 
 var server = app.listen(80, 'builder.websites.ca', function() {
 	console.log('Listening, %s:%d', server.address().address, server.address().port);
